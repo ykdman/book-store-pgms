44a3893ff1e0d9145a0f29e2e3a9d04f48dba031
YKDman <dbsrudejr300@gmail.com>
2024-07-23 23:08:16 +0900

    http 리팩토링

--- src/shared/api/http.ts
+++ src/shared/api/http.ts
@@ -35,3 +35,34 @@ export const createClient = (config?: AxiosRequestConfig) => {
 };
 
 export const httpClient = createClient();
+
+type RequestMethod = "get" | "post" | "put" | "delete";
+
+// 공통 요청 부분
+export const requestHandler = async <T>(
+  method: RequestMethod,
+  url: string,
+  payload?: T
+) => {
+  let response;
+  switch (method) {
+    case "post": {
+      response = await httpClient.post(url, payload);
+      break;
+    }
+    case "get": {
+      response = await httpClient.get(url);
+      break;
+    }
+    case "delete": {
+      response = await httpClient.delete(url);
+      break;
+    }
+    case "put": {
+      response = await httpClient.put(url, payload);
+      break;
+    }
+  }
+
+  return response.data;
+};
--- src/shared/api/order.api.ts
+++ src/shared/api/order.api.ts
@@ -1,14 +1,17 @@
 import { OrderDetailItem, OrderSheet } from "../models/order.model";
-import { httpClient } from "./http";
+import { httpClient, requestHandler } from "./http";
 
 export const order = async (orderData: OrderSheet) => {
-  const response = await httpClient.post("/orders", orderData);
-  return response.data;
+  // const response = await httpClient.post("/orders", orderData);
+  // return response.data;
+
+  return await requestHandler("post", "/orders", orderData);
 };
 
 export const fetchOrders = async () => {
-  const response = await httpClient.get("/orders");
-  return response.data;
+  // const response = await httpClient.get("/orders");
+  // return response.data;
+  return await requestHandler("get", "/orders");
 };
 
 export const fetchOrder = async (orderId: number) => {
